449d42cb43dac1e9712c5d5cf9a17c27622c5266
Chiara Nguyen Ba <106277568+chiaranb@users.noreply.github.com>
2023-02-27 19:43:04 +0100

    Update project.vhd

--- project.vhd
+++ project.vhd
@@ -1,1 +1,40 @@
+----------------------------------------------------------------------------------
+-- Prova Finale (Progetto di Reti Logiche) AA 2022-2023
+-- Chiara Thien Thao Nguyen Ba CodicePersona 10727985
+-- Flavia Nicotri CodicePersona
+-- Prof. Fabio Salice
+--
+-- Module Name: project_reti_logiche - Behavioral
+-- Project Name: CodicePersona.vhd
+----------------------------------------------------------------------------------
 
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity project_reti_logiche is
+    port (
+        i_clk : in std_logic;
+        i_rst : in std_logic;
+        i_start : in std_logic;
+        i_w : in std_logic;
+        o_z0 : out std_logic_vector(7 downto 0);
+        o_z1 : out std_logic_vector(7 downto 0);
+        o_z2 : out std_logic_vector(7 downto 0);
+        o_z3 : out std_logic_vector(7 downto 0);
+        o_done : out std_logic;
+        o_mem_addr : out std_logic_vector(15 downto 0);
+        i_mem_data : in std_logic_vector(7 downto 0);
+        o_mem_we : out std_logic;
+        o_mem_en : out std_logic
+    );
+end project_reti_logiche;
+
+architecture Behavioral of project_reti_logiche is
+
+begin
+
+
+end Behavioral;
