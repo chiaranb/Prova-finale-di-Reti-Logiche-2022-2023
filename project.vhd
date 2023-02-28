cb4bff133bd021b8672336a49f88f419ad1f5e83
Chiara Nguyen Ba <106277568+chiaranb@users.noreply.github.com>
2023-02-28 19:30:34 +0100

    Update project.vhd
    Ho inserito il Datapath component, da rivedere i segnali da inserire all'inizio

--- project.vhd
+++ project.vhd
@@ -15,26 +15,80 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity project_reti_logiche is
-    port (
-        i_clk : in std_logic;
-        i_rst : in std_logic;
-        i_start : in std_logic;
-        i_w : in std_logic;
-        o_z0 : out std_logic_vector(7 downto 0);
-        o_z1 : out std_logic_vector(7 downto 0);
-        o_z2 : out std_logic_vector(7 downto 0);
-        o_z3 : out std_logic_vector(7 downto 0);
-        o_done : out std_logic;
-        o_mem_addr : out std_logic_vector(15 downto 0);
-        i_mem_data : in std_logic_vector(7 downto 0);
-        o_mem_we : out std_logic;
-        o_mem_en : out std_logic
+    port (  i_clk : in STD_LOGIC;
+            i_rst : in STD_LOGIC;
+            i_start : in STD_LOGIC;
+            i_w : in STD_LOGIC;
+            o_z0 : out STD_LOGIC_VECTOR (7 downto 0);
+            o_z1 : out STD_LOGIC_VECTOR (7 downto 0);
+            o_z2 : out STD_LOGIC_VECTOR (7 downto 0);
+            o_z3 : out STD_LOGIC_VECTOR (7 downto 0);
+            o_done : out STD_LOGIC;
+            o_mem_addr : out STD_LOGIC_VECTOR (15 downto 0);
+            i_mem_data : in STD_LOGIC_VECTOR (7 downto 0);
+            o_mem_we : out STD_LOGIC;
+            o_mem_en : out STD_LOGIC;
     );
 end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
+    --Datapath component
+    component datapath is
+    Port ( i_clk : in STD_LOGIC;
+           i_rst : in STD_LOGIC;
+           i_mem_data : in STD_LOGIC_VECTOR (7 downto 0);
+           o_z0 : out STD_LOGIC_VECTOR (7 downto 0);
+           o_z1 : out STD_LOGIC_VECTOR (7 downto 0);
+           o_z2 : out STD_LOGIC_VECTOR (7 downto 0);
+           o_z3 : out STD_LOGIC_VECTOR (7 downto 0);
+           --o_done: out STD_LOGIC;
+          
+end component;  
+          
+  --Registri
+          signal o_regCh : STD_LOGIC_VECTOR (1 downto 0);
+          signal rCh_load : STD_LOGIC;
+          
+          signal o_regAdd : STD_LOGIC_VECTOR (15 downto 0);
+          signal rAdd_load : STD_LOGIC;
+          
+          signal o_regZ0 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ0_load : STD_LOGIC;
+          
+          signal o_regZ1: STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ1_load : STD_LOGIC;
+          
+          signal o_regZ2 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ2_load : STD_LOGIC;
+          
+          signal o_regZ3 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ3_load : STD_LOGIC;
+   --Mux
+          signal mux_regAdd : STD_LOGIC_VECTOR (15 downto 0);
+          signal rAdd_sel :  STD_LOGIC;
+                    
+          signal mux_regZ0 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ0_sel :  STD_LOGIC;
+          
+          signal mux_regZ1 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ1_sel :  STD_LOGIC;
+          
+          signal mux_regZ2 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ2_sel :  STD_LOGIC;
+          
+          signal mux_regZ3 : STD_LOGIC_VECTOR (7 downto 0);
+          signal rZ3_sel :  STD_LOGIC;
+          
+    --Demultiplexer
+          signal demux : STD_LOGIC_VECTOR (7 downto 0);
+          signal demux_sel : STD_LOGIC_VECTOR (1 downto 0);
 
+type Stato is ( );
+signal cur_state, next_state : Stato;
+          
 begin
-
-
+--
+--Datapath 
+--
+          
 end Behavioral;
